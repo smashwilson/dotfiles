319d5bdf8e2c45b8e786e85c04654849f41e270d
Ash Wilson <ash.wilson@rackspace.com>
2013-10-28 16:24:56 +0000

    Fixups in ruby/.

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,11 +3,11 @@
 if test ! $(which rbenv)
 then
   echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+  git clone https://github.com/sstephenson/rbenv.git ${HOME}/.rbenv
 fi
 
 if test ! $(which ruby-build)
 then
   echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
+  git clone https://github.com/sstephenson/ruby-build.git ${HOME}/.rbenv/plugins/ruby-build
 fi
