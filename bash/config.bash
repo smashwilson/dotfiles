c7ba1b9ef02f387b63f95da5d2cb0767d091eef1
Ash Wilson <ash.wilson@rackspace.com>
2013-10-28 16:18:58 +0000

    More customizations in bash/.

--- bash/config.bash
+++ bash/config.bash
@@ -4,26 +4,14 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
-
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
 # don't put duplicate lines or lines starting with a space in the history.
 HISTCONTROL=ignoreboth
 
 shopt -s histappend
-
 HISTSIZE=10000
 SAVEHIST=10000
 
 shopt -s checkwinsize
+shopt -u direxpand
 
 export EDITOR=vim
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
