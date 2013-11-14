17ea5e1caedff055ac6dd734325aea0e153a8392
Ash Wilson <ash.wilson@rackspace.com>
2013-11-14 12:44:00 -0500

    Persistent ssh-agent.

---
+++ bash/sshagent.bash
@@ -0,0 +1,16 @@
+# Set up a persistent, cross-session ssh-agent.
+#
+# http://superuser.com/questions/141044/sharing-the-same-ssh-agent-among-multiple-login-sessions
+
+check_ssh_agent() {
+    [ -S "$SSH_AUTH_SOCK" ] && { ssh-add -l >& /dev/null || [ $? -ne 2 ]; }
+}
+
+# attempt to connect to a running agent
+check_ssh_agent || export SSH_AUTH_SOCK="$(< ~/.tmp/ssh-agent.env)"
+
+# if agent.env data is invalid, start a new one
+check_ssh_agent || {
+    eval "$(ssh-agent -s)" > /dev/null
+    echo "$SSH_AUTH_SOCK" > ~/.tmp/ssh-agent.env
+}
