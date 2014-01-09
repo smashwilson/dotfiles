df64a4863b57e73fc0a34eef0dd0202b1033743f
Ash Wilson <ash.wilson@rackspace.com>
2014-01-09 09:05:45 -0500

    Print a friendlier message without an ssh-agent.

--- bash/sshagent.bash
+++ bash/sshagent.bash
@@ -6,11 +6,15 @@ check_ssh_agent() {
     [ -S "$SSH_AUTH_SOCK" ] && { ssh-add -l >& /dev/null || [ $? -ne 2 ]; }
 }
 
-# attempt to connect to a running agent
-check_ssh_agent || export SSH_AUTH_SOCK="$(< ~/.tmp/ssh-agent.env)"
+if [ -n "$SSH_AUTH_SOCK" ]; then
+  # attempt to connect to a running agent
+  check_ssh_agent || export SSH_AUTH_SOCK="$(< ~/.tmp/ssh-agent.env)"
 
-# if agent.env data is invalid, start a new one
-check_ssh_agent || {
-    eval "$(ssh-agent -s)" > /dev/null
-    echo "$SSH_AUTH_SOCK" > ~/.tmp/ssh-agent.env
-}
+  # if agent.env data is invalid, start a new one
+  check_ssh_agent || {
+      eval "$(ssh-agent -s)" > /dev/null
+      echo "$SSH_AUTH_SOCK" > ~/.tmp/ssh-agent.env
+  }
+else
+  echo "SSH agent not forwarded."
+fi
