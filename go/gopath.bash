0c3c7fdbf1eeccc9e9712c23775c45dca829ad71
Ash Wilson <smashwilson@gmail.com>
2014-11-03 08:10:10 -0500

    GOPATH stuff.

--- go/gopath.bash
+++ go/gopath.bash
@@ -1,4 +1,8 @@
 export GOPATH=${HOME}/go
+export GOROOT=`go env GOROOT`
 
 # add the ${GOPATH} binary directory to the path.
 export PATH=${PATH}:${GOPATH}/bin
+
+# add the ${GOROOT} binary directory to the path.
+export PATH=${PATH}:${GOROOT}/bin
