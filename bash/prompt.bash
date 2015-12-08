3538d9bf5f78afaaf1f75909ada177a95348a11c
Ash Wilson <smashwilson@gmail.com>
2015-12-08 09:35:47 -0500

    Fiddle with my bash prompt colors

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -4,4 +4,27 @@ export GIT_PS1_SHOWDIRTYSTATE="1"
 export GIT_PS1_SHOWUNTRACKEDFILES="1"
 export GIT_PS1_SHOWUPSTREAM="auto"
 
-export PS1="${GREEN}\u@\h${CYAN} \w${YELLOW}\$(__git_ps1)${CYAN} \$${RESET} "
+__last_exit()
+{
+  if [ $PS1_EXITCODE -eq 0 ]; then
+    echo -n "${GREEN} "
+  else
+    echo -n "${RED} [$?] "
+  fi
+}
+
+__set_prompt()
+{
+  PS1_EXITCODE=$?
+
+  PS1=""
+
+  [ -z "${PS1_NOWHO}" ] && PS1="${PS1}${GREEN}\u "
+
+  PS1="${PS1}${CYAN}\w"
+  PS1="${PS1}${YELLOW}$(__git_ps1)"
+  PS1="${PS1}$(__last_exit)\$"
+  PS1="${PS1}${RESET} "
+}
+
+export PROMPT_COMMAND=__set_prompt
