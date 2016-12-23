d914b639c1edde25ddc40a8e09ff5c9f9b60296a
Ash Wilson <smashwilson@gmail.com>
2016-12-23 11:39:27 -0500

    Generalize title()

--- bash/title.bash
+++ bash/title.bash
@@ -1,12 +1,14 @@
 # From http://dotfiles.org/~_why/.zshrc
 # Sets the window title nicely no matter where you are
 function title() {
+  local TITLE=${1:-$(basename $(pwd))}
+
   case $TERM in
   screen)
-    echo -en "\ek$a:$1\e\\" # screen title (in ^A")
+    echo -en "\ek$a:${TITLE}\e\\" # screen title (in ^A")
     ;;
   xterm*|rxvt)
-    echo -en "\033]0;$@\007" # plain xterm title ($3 for pwd)
+    echo -en "\033]0;${TITLE}\007" # plain xterm title ($3 for pwd)
     ;;
   esac
 }
