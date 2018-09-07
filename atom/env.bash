a13fdbef66541d217285957e6959772c9ed5f79b
Ash Wilson <smashwilson@github.com>
2018-09-07 07:50:18 -0400

    "atomlive" and "atomunlive" functions to manipulate dev resources

--- atom/env.bash
+++ atom/env.bash
@@ -1,2 +1,14 @@
+#!/bin/bash
+
 export ATOM_REPOS_HOME=${HOME}/src/
-export ATOM_DEV_RESOURCE_PATH=${HOME}/src/atom/atom
+export ATOM_SRC=${HOME}/src/atom/atom
+
+atomlive() {
+  export ATOM_DEV_RESOURCE_PATH=${ATOM_SRC}
+  printf "Atom dev mode will load source from %b%s%b.\n" "${CYAN}" "${ATOM_SRC}" "${RESET}"
+}
+
+atomunlive() {
+  unset ATOM_DEV_RESOURCE_PATH
+  printf "Atom dev mode will load shipped source.\n"
+}
