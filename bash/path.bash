0f9a01f690863230f357b42dd90652d6ec5207c1
Ash Wilson <smashwilson@github.com>
2021-06-02 18:17:56 +0000

    Update $DOT to $DOTFILES

--- bash/path.bash
+++ bash/path.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOT/bin:$PATH"
+export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 
 lspath() {
   echo "${PATH}" | tr ':' '\n'
