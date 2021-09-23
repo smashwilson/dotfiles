bec7bffe914f1d5daa6b43adb43b50c70b1d9d94
Ash Wilson <smashwilson@github.com>
2021-09-23 10:26:26 -0400

    Add the GOPATH on codespaces

--- bash/path.bash
+++ bash/path.bash
@@ -2,6 +2,10 @@
 
 export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 
+if has go && [ "${CODESPACES:-}" = "true" ]; then
+  export PATH="${PATH}:$(go env GOPATH)/bin"
+fi
+
 lspath() {
   echo "${PATH}" | tr ':' '\n'
 }
