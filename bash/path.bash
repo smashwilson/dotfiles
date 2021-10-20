35633e2adc3bc308f32f005e943e2c5ad3936569
Ash Wilson <smashwilson@github.com>
2021-10-20 14:43:43 +0000

    ./bin gives me warnings, meh

--- bash/path.bash
+++ bash/path.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 
 if has go && [ "${CODESPACES:-}" = "true" ]; then
   export PATH="${PATH}:$(go env GOPATH)/bin"
