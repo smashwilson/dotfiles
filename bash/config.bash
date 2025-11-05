1522d9378fb9ba2408a92c5a1f48122e39565934
Ash Wilson <smashwilson@github.com>
2025-11-05 09:19:53 -0500

    Remove overwritten PS1 setting

--- bash/config.bash
+++ bash/config.bash
@@ -1,9 +1,3 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
-
 # don't put duplicate lines or lines starting with a space in the history.
 HISTCONTROL=ignoreboth
 
